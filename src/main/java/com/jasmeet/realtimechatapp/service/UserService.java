6e8e5dd3ed9a051f5e4a8544c13eb5fdff244061
harshitsharma2005 <work.harshitsharma8899@gmail.com>
2026-09-21 13:10:46 +0530

    Implement authentication, security and real-time messaging

---
+++ src/main/java/com/jasmeet/realtimechatapp/service/UserService.java
@@ -0,0 +1,24 @@
+package com.jasmeet.realtimechatapp.service;
+
+import com.jasmeet.realtimechatapp.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public boolean userExists(String username) {
+        return userRepository.existsByUsername(username);
+    }
+
+    @jakarta.annotation.PostConstruct
+    public void resetUsersOnlineStatusOnStartup() {
+        userRepository.resetAllUsersToOffline();
+    }
+
+    public void setUserOnlineStatus(String username, boolean isOnline) {
+        userRepository.updateUserOnlineStatus(username, isOnline);
+    }
+}
